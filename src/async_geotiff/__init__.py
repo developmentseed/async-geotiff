d78115b9ce65e267100ef6635b36ad87181b4081
Kyle Barron <kyle@developmentseed.org>
2026-01-27 18:11:19 -0500

    docstring

--- src/async_geotiff/__init__.py
+++ src/async_geotiff/__init__.py
@@ -1,3 +1,8 @@
+"""Async GeoTIFF and [Cloud-Optimized GeoTIFF][cogeo] (COG) reader for Python.
+
+[cogeo]: https://cogeo.org/
+"""
+
 from ._geotiff import GeoTIFF
 from ._overview import Overview
 from ._version import __version__
