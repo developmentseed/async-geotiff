860fbf8f7b680aabec8a1872f934498a45a839cc
Kyle Barron <kyle@developmentseed.org>
2026-01-27 16:41:35 -0500

    feat: Create TileMatrixSet from GeoTIFF metadata

--- src/async_geotiff/_tms.py
+++ src/async_geotiff/_tms.py
@@ -1,1 +1,102 @@
 """Generate a Tile Matrix Set from a GeoTIFF file, using morecantile."""
+
+from __future__ import annotations
+
+import math
+from uuid import uuid4
+
+import morecantile.models
+import pyproj
+from morecantile.commons import BoundingBox
+from morecantile.models import CRSWKT, CRSUri, TileMatrix, TileMatrixSet, TMSBoundingBox
+from morecantile.utils import meters_per_unit
+
+from async_geotiff import GeoTIFF
+
+SCREEN_PIXEL_SIZE = 0.28e-3
+
+
+def generate_tms(geotiff: GeoTIFF, *, id: str = str(uuid4())) -> TileMatrixSet:
+    """Generate a Tile Matrix Set from a GeoTIFF file.
+
+    Args:
+        geotiff: The GeoTIFF file to generate the TMS from.
+    """
+    bounds = geotiff.bounds
+    crs = geotiff.crs
+    tr = geotiff.transform
+    # TODO: use blocksize from geotiff
+    blockxsize = 256
+    blockysize = 256
+
+    mpu = meters_per_unit(crs)
+
+    tile_matrices: list[TileMatrix] = [
+        TileMatrix(
+            **{
+                "id": "0",
+                "scaleDenominator": tr.a * mpu / SCREEN_PIXEL_SIZE,
+                "cellSize": tr.a,
+                "pointOfOrigin": [tr.c, tr.f],
+                "tileWidth": blockxsize,
+                "tileHeight": blockysize,
+                "matrixWidth": math.ceil(geotiff.width / blockxsize),
+                "matrixHeight": math.ceil(geotiff.height / blockysize),
+            }
+        )
+    ]
+
+    for idx, overview in enumerate(geotiff.overviews):
+        overview_tr = overview.transform
+        # TODO: get from overview IFD
+        blockxsize, blockysize = 256, 256
+        tile_matrices.append(
+            TileMatrix(
+                **{
+                    "id": f"{idx + 1}",
+                    "scaleDenominator": overview_tr.a * mpu / SCREEN_PIXEL_SIZE,
+                    "cellSize": overview_tr.a,
+                    "pointOfOrigin": [overview_tr.c, overview_tr.f],
+                    "tileWidth": blockxsize,
+                    "tileHeight": blockysize,
+                    "matrixWidth": math.ceil(overview.width / blockxsize),
+                    "matrixHeight": math.ceil(overview.height / blockysize),
+                }
+            )
+        )
+
+    bbox = BoundingBox(*bounds)
+    tms_crs = parse_crs(crs)
+
+    return TileMatrixSet(
+        title="Generated TMS",
+        id=id,
+        crs=tms_crs,
+        boundingBox=TMSBoundingBox(
+            lowerLeft=[bbox.left, bbox.bottom],
+            upperRight=[bbox.right, bbox.top],
+            crs=tms_crs,
+        ),
+        tileMatrices=tile_matrices,
+    )
+
+
+def parse_crs(crs: pyproj.CRS) -> morecantile.models.CRSUri | morecantile.models.CRSWKT:
+    """Parse a pyproj CRS into a morecantile CRSUri or CRSWKT.
+
+    Args:
+        crs: The pyproj CRS to parse.
+    """
+    if authority_code := crs.to_authority(min_confidence=20):
+        authority, code = authority_code
+        version = "0"
+        # if we have a version number in the authority, split it out
+        if "_" in authority:
+            authority, version = authority.split("_")
+
+        return CRSUri(
+            uri=f"http://www.opengis.net/def/crs/{authority}/{version}/{code}"
+        )
+
+    else:
+        return CRSWKT(wkt=crs.to_json_dict())
