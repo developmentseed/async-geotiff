5df454b2553d11d935d81db84ee768fcbb426481
Kyle Barron <kyle@developmentseed.org>
2026-01-27 14:25:08 -0500

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,32 @@ from __future__ import annotations
 
 from contextlib import contextmanager
 from pathlib import Path
-from typing import TYPE_CHECKING, Generator
+from typing import TYPE_CHECKING, Generator, Literal, Protocol
 
 import pytest
 import rasterio
+from async_tiff import TIFF
 from async_tiff.store import LocalStore
 
 from async_geotiff import GeoTIFF
 
 if TYPE_CHECKING:
     from rasterio.io import DatasetReader
+
+Variant = Literal["rasterio", "nlcd"]
+
+
+class LoadGeoTIFF(Protocol):
+    async def __call__(
+        self, name: str, *, variant: Variant = "rasterio"
+    ) -> GeoTIFF: ...
+
+
+class LoadRasterio(Protocol):
+    @contextmanager
+    def __call__(
+        self, name: str, *, variant: Variant = "rasterio"
+    ) -> Generator[DatasetReader, None, None]: ...
 
 
 @pytest.fixture(scope="session")
@@ -31,8 +47,17 @@ def fixture_store(root_dir) -> LocalStore:
 
 @pytest.fixture
 def load_geotiff(fixture_store):
-    async def _load(name: str) -> GeoTIFF:
-        path = f"geotiff-test-data/rasterio_generated/fixtures/{name}.tif"
+    async def _load(name: str, *, variant: Variant = "rasterio") -> GeoTIFF:
+        path = "geotiff-test-data/"
+
+        if variant == "rasterio":
+            path += "rasterio_generated/fixtures/"
+        elif variant == "nlcd":
+            path += "real_data/nlcd/"
+        else:
+            raise ValueError(f"Unknown variant: {variant}")
+
+        path = f"{path}{name}.tif"
         return await GeoTIFF.open(path=path, store=fixture_store)
 
     return _load
@@ -41,8 +66,18 @@ def load_geotiff(fixture_store):
 @pytest.fixture
 def load_rasterio(root_dir):
     @contextmanager
-    def _load(name: str) -> Generator[DatasetReader, None, None]:
-        path = f"{root_dir}/fixtures/geotiff-test-data/rasterio_generated/fixtures/{name}.tif"
+    def _load(
+        name: str, *, variant: Variant = "rasterio"
+    ) -> Generator[DatasetReader, None, None]:
+        path = f"{root_dir}/fixtures/"
+        if variant == "rasterio":
+            path += "rasterio_generated/fixtures/"
+        elif variant == "nlcd":
+            path += "real_data/nlcd/"
+        else:
+            raise ValueError(f"Unknown variant: {variant}")
+
+        path = f"{path}{name}.tif"
         with rasterio.open(path) as ds:
             yield ds
 
