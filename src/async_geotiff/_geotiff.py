36e7bd1d9e5cfbea512f63d4cbc0ee854b7c4fb2
Kyle Barron <kyle@developmentseed.org>
2026-01-27 17:58:49 -0500

    lints

--- src/async_geotiff/_geotiff.py
+++ src/async_geotiff/_geotiff.py
@@ -13,6 +13,8 @@ from async_geotiff._overview import Overview
 from async_geotiff.enums import Compression, Interleaving
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
+
     import pyproj
     from async_tiff import GeoKeyDirectory, ImageFileDirectory, ObspecInput
     from async_tiff.store import ObjectStore
@@ -42,7 +44,6 @@ class GeoTIFF:
 
     def __init__(self, tiff: TIFF) -> None:
         """Create a GeoTIFF from an existing TIFF instance."""
-
         first_ifd = tiff.ifds[0]
         gkd = first_ifd.geo_key_directory
 
@@ -99,7 +100,7 @@ class GeoTIFF:
         *,
         store: ObjectStore | ObspecInput,
         prefetch: int = 32768,
-        multiplier: int | float = 2.0,
+        multiplier: float = 2.0,
     ) -> Self:
         """Open a new GeoTIFF.
 
@@ -114,38 +115,44 @@ class GeoTIFF:
 
         Returns:
             A TIFF instance.
+
         """
         tiff = await TIFF.open(
-            path=path, store=store, prefetch=prefetch, multiplier=multiplier
+            path=path,
+            store=store,
+            prefetch=prefetch,
+            multiplier=multiplier,
         )
         return cls(tiff)
 
     @property
     def block_shapes(self) -> list[tuple[int, int]]:
-        """An ordered list of block shapes for each bands
+        """An ordered list of block shapes for each bands.
 
         Shapes are tuples and have the same ordering as the dataset's shape:
 
         - (count of image rows, count of image columns).
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def block_size(self, bidx: int, i: int, j: int) -> int:
-        """Returns the size in bytes of a particular block.
+        """Return the size in bytes of a particular block.
 
         Args:
             bidx: Band index, starting with 1.
             i: Row index of the block, starting with 0.
             j: Column index of the block, starting with 0.
+
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @cached_property
     def bounds(self) -> tuple[float, float, float, float]:
-        """Returns the bounds of the dataset in the units of its coordinate reference system.
+        """Return the bounds of the dataset in the units of its CRS.
 
         Returns:
             (lower left x, lower left y, upper right x, upper right y)
+
         """
         transform = self.transform
         (left, top) = transform * (0, 0)
@@ -157,10 +164,10 @@ class GeoTIFF:
     def colorinterp(self) -> list[str]:
         """The color interpretation of each band in index order."""
         # TODO: we should return an enum here. The enum should match rasterio.
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def colormap(self, bidx: int) -> dict[int, tuple[int, int, int]]:
-        """Returns a dict containing the colormap for a band.
+        """Return a dict containing the colormap for a band.
 
         Args:
             bidx: The 1-based index of the band whose colormap will be returned.
@@ -176,7 +183,7 @@ class GeoTIFF:
                 If no band exists for the provided index.
 
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @property
     def compression(self) -> Compression:
@@ -186,12 +193,12 @@ class GeoTIFF:
         # the base image?
         # Should we diverge from rasterio and not have this as a property returning a
         # single string?
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @property
     def count(self) -> int:
         """The number of raster bands in the full image."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @cached_property
     def crs(self) -> pyproj.CRS:
@@ -203,7 +210,7 @@ class GeoTIFF:
         """The data types of each band in index order."""
         # TODO: not sure what the return type should be. Perhaps we should define a
         # `DataType` enum?
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @property
     def height(self) -> int:
@@ -214,7 +221,7 @@ class GeoTIFF:
         self,
         x: float,
         y: float,
-        op=None,
+        op: Callable[[float, float], tuple[int, int]] | None = None,
     ) -> tuple[int, int]:
         """Get the (row, col) index of the pixel containing (x, y).
 
@@ -227,11 +234,12 @@ class GeoTIFF:
 
         Returns:
             (row index, col index)
+
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def indexes(self) -> list[int]:
-        """The 1-based indexes of each band in the dataset
+        """Return the 1-based indexes of each band in the dataset.
 
         For a 3-band dataset, this property will be [1, 2, 3].
         """
@@ -242,12 +250,12 @@ class GeoTIFF:
         """The interleaving scheme of the dataset."""
         # TODO: Should return an enum.
         # https://rasterio.readthedocs.io/en/stable/api/rasterio.enums.html#rasterio.enums.Interleaving
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @property
     def is_tiled(self) -> bool:
         """Check if the dataset is tiled."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @property
     def nodata(self) -> float | None:
@@ -268,11 +276,11 @@ class GeoTIFF:
         """The photometric interpretation of the dataset."""
         # TODO: should return enum
         # https://rasterio.readthedocs.io/en/stable/api/rasterio.enums.html#rasterio.enums.PhotometricInterp
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @cached_property
     def res(self) -> tuple[float, float]:
-        """Returns the (width, height) of pixels in the units of its coordinate reference system."""
+        """Return the (width, height) of pixels in the units of its CRS."""
         transform = self.transform
         return (transform.a, -transform.e)
 
@@ -283,9 +291,10 @@ class GeoTIFF:
 
     @cached_property
     def transform(self) -> Affine:
-        """The dataset's georeferencing transformation matrix
+        """Return the dataset's georeferencing transformation matrix.
 
-        This transform maps pixel row/column coordinates to coordinates in the dataset's coordinate reference system.
+        This transform maps pixel row/column coordinates to coordinates in the dataset's
+        CRS.
         """
         if (tie_points := self._primary_ifd.model_tiepoint) and (
             model_scale := self._primary_ifd.model_pixel_scale
@@ -341,7 +350,7 @@ class GeoTIFF:
         The pixel's center is returned by default, but a corner can be returned
         by setting `offset` to one of `"ul"`, `"ur"`, `"ll"`, `"lr"`.
 
-        Parameters:
+        Args:
             row: Pixel row.
             col: Pixel column.
             offset: Determines if the returned coordinates are for the center of the
@@ -383,12 +392,9 @@ def has_geokeys(ifd: ImageFileDirectory) -> bool:
 
 def is_mask_ifd(ifd: ImageFileDirectory) -> bool:
     """Check if an IFD is a mask IFD."""
-    if (
+    return (
         ifd.compression == Compression.deflate
         and ifd.new_subfile_type is not None
         and ifd.new_subfile_type & 4 != 0
         and ifd.photometric_interpretation == PhotometricInterpretation.TransparencyMask
-    ):
-        return True
-
-    return False
+    )
