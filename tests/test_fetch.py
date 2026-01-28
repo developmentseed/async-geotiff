1ba7d9303cc270208d9fb13182fbbef957a3c8a3
Kyle Barron <kyle@developmentseed.org>
2026-01-27 19:37:10 -0500

    Add test for tile fetch

---
+++ tests/test_fetch.py
@@ -0,0 +1,28 @@
+"""Test fetching tiles from a GeoTIFF."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+import pytest
+from rasterio.windows import Window
+
+if TYPE_CHECKING:
+    from .conftest import LoadGeoTIFF, LoadRasterio
+
+
+@pytest.mark.asyncio
+async def test_fetch(load_geotiff: LoadGeoTIFF, load_rasterio: LoadRasterio) -> None:
+    name = "uint8_rgb_deflate_block64_cog"
+
+    geotiff = await load_geotiff(name)
+
+    tile = await geotiff.fetch_tile(0, 0)
+
+    window = Window(0, 0, geotiff.tile_width, geotiff.tile_height)
+    with load_rasterio(name) as rasterio_ds:
+        rasterio_data = rasterio_ds.read(window=window)
+
+    np.testing.assert_array_equal(tile.data, rasterio_data)
+    assert tile.crs == geotiff.crs
