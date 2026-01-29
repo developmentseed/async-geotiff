3a13d31f985fd895a6bdaba083c00386fa1ee99d
Kyle Barron <kyle@developmentseed.org>
2026-01-29 12:20:12 -0500

    Update tests

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -9,19 +9,37 @@ import pytest
 from rasterio.windows import Window
 
 if TYPE_CHECKING:
-    from .conftest import LoadGeoTIFF, LoadRasterio
+    from .conftest import LoadGeoTIFF, LoadRasterio, Variant
 
 
 @pytest.mark.asyncio
-async def test_fetch(load_geotiff: LoadGeoTIFF, load_rasterio: LoadRasterio) -> None:
-    name = "uint8_rgb_deflate_block64_cog"
-
-    geotiff = await load_geotiff(name)
+@pytest.mark.parametrize(
+    ("file_name", "variant"),
+    [
+        # TODO: support LERC
+        # https://github.com/developmentseed/async-geotiff/issues/34
+        # ("float32_1band_lerc_block32", "rasterio"), # noqa: ERA001
+        ("uint16_1band_lzw_block128_predictor2", "rasterio"),
+        ("uint8_rgb_deflate_block64_cog", "rasterio"),
+        ("uint8_1band_deflate_block128_unaligned", "rasterio"),
+        # TODO: debug incorrect data length
+        # https://github.com/developmentseed/async-tiff/issues/202
+        # ("maxar_opendata_yellowstone_visual", "vantor"), # noqa: ERA001
+        ("nlcd_landcover", "nlcd"),
+    ],
+)
+async def test_fetch(
+    load_geotiff: LoadGeoTIFF,
+    load_rasterio: LoadRasterio,
+    file_name: str,
+    variant: Variant,
+) -> None:
+    geotiff = await load_geotiff(file_name, variant=variant)
 
     tile = await geotiff.fetch_tile(0, 0)
 
     window = Window(0, 0, geotiff.tile_width, geotiff.tile_height)
-    with load_rasterio(name) as rasterio_ds:
+    with load_rasterio(file_name, variant=variant) as rasterio_ds:
         rasterio_data = rasterio_ds.read(window=window)
 
     np.testing.assert_array_equal(tile.data, rasterio_data)
@@ -29,19 +47,34 @@ async def test_fetch(load_geotiff: LoadGeoTIFF, load_rasterio: LoadRasterio) -> 
 
 
 @pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("file_name", "variant"),
+    [
+        # TODO: support LERC
+        # https://github.com/developmentseed/async-geotiff/issues/34
+        # ("float32_1band_lerc_block32", "rasterio"), # noqa: ERA001
+        ("uint16_1band_lzw_block128_predictor2", "rasterio"),
+        ("uint8_rgb_deflate_block64_cog", "rasterio"),
+        ("uint8_1band_deflate_block128_unaligned", "rasterio"),
+        # TODO: debug incorrect data length
+        # https://github.com/developmentseed/async-tiff/issues/202
+        # ("maxar_opendata_yellowstone_visual", "vantor"), # noqa: ERA001
+        ("nlcd_landcover", "nlcd"),
+    ],
+)
 async def test_fetch_overview(
     load_geotiff: LoadGeoTIFF,
     load_rasterio: LoadRasterio,
+    file_name: str,
+    variant: Variant,
 ) -> None:
-    name = "uint8_rgb_deflate_block64_cog"
-
-    geotiff = await load_geotiff(name)
+    geotiff = await load_geotiff(file_name, variant=variant)
     overview = geotiff.overviews[0]
 
     tile = await overview.fetch_tile(0, 0)
 
     window = Window(0, 0, overview.tile_width, overview.tile_height)
-    with load_rasterio(name, OVERVIEW_LEVEL=0) as rasterio_ds:
+    with load_rasterio(file_name, variant=variant, OVERVIEW_LEVEL=0) as rasterio_ds:
         rasterio_data = rasterio_ds.read(window=window)
 
     np.testing.assert_array_equal(tile.data, rasterio_data)
