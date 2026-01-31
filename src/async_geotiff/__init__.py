b186aa7f259089c89c384ab496d40f47bd3a1ebf
Kyle Barron <kyle@developmentseed.org>
2026-01-30 22:48:17 -0500

    Create Tile dataclass

--- src/async_geotiff/__init__.py
+++ src/async_geotiff/__init__.py
@@ -6,7 +6,8 @@
 from ._array import Array
 from ._geotiff import GeoTIFF
 from ._overview import Overview
+from ._tile import Tile
 from ._version import __version__
 from .windows import Window
 
-__all__ = ["Array", "GeoTIFF", "Overview", "__version__"]
+__all__ = ["Array", "GeoTIFF", "Overview", "Tile", "Window", "__version__"]
