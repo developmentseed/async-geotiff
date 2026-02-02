61cbd0031c41c24cc19af20602383e4e7bd3e20f
Kyle Barron <kyle@developmentseed.org>
2026-02-02 10:32:17 -0500

    feat: Cleanup pass for 0.1 (#62)

--- src/async_geotiff/enums.py
+++ src/async_geotiff/enums.py
@@ -1,6 +1,6 @@
 """Enums used by async_geotiff."""
 
-from enum import Enum, IntEnum
+from enum import Enum
 
 # ruff: noqa: D101
 
@@ -13,47 +13,39 @@ class Compression(Enum):
     in this enum.
     """
 
-    jpeg = "JPEG"
-    lzw = "LZW"
-    packbits = "PACKBITS"
-    deflate = "DEFLATE"
-    ccittrle = "CCITTRLE"
-    ccittfax3 = "CCITTFAX3"
-    ccittfax4 = "CCITTFAX4"
-    lzma = "LZMA"
-    none = "NONE"
-    zstd = "ZSTD"
-    lerc = "LERC"
-    lerc_deflate = "LERC_DEFLATE"
-    lerc_zstd = "LERC_ZSTD"
-    webp = "WEBP"
-    jpeg2000 = "JPEG2000"
+    JPEG = "JPEG"
+    LZW = "LZW"
+    PACKBITS = "PACKBITS"
+    DEFLATE = "DEFLATE"
+    CCITTRLE = "CCITTRLE"
+    CCITTFAX3 = "CCITTFAX3"
+    CCITTFAX4 = "CCITTFAX4"
+    LZMA = "LZMA"
+    NONE = "NONE"
+    ZSTD = "ZSTD"
+    LERC = "LERC"
+    LERC_DEFLATE = "LERC_DEFLATE"
+    LERC_ZSTD = "LERC_ZSTD"
+    WEBP = "WEBP"
+    JPEG2000 = "JPEG2000"
+    UNCOMPRESSED = "UNCOMPRESSED"
 
 
 # https://github.com/rasterio/rasterio/blob/2d79e5f3a00e919ecaa9573adba34a78274ce48c/rasterio/enums.py#L177-L182
 class Interleaving(Enum):
-    pixel = "PIXEL"
-    line = "LINE"
-    band = "BAND"
-    #: tile requires GDAL 3.11+
-    tile = "TILE"
-
-
-# https://github.com/rasterio/rasterio/blob/2d79e5f3a00e919ecaa9573adba34a78274ce48c/rasterio/enums.py#L185-L189
-class MaskFlags(IntEnum):
-    all_valid = 1
-    per_dataset = 2
-    alpha = 4
-    nodata = 8
+    PIXEL = "PIXEL"
+    BAND = "BAND"
+    # TODO: Support GDAL's new TILE interleaving option
+    # https://gdal.org/en/stable/drivers/raster/cog.html#general-creation-options
 
 
 # https://github.com/rasterio/rasterio/blob/2d79e5f3a00e919ecaa9573adba34a78274ce48c/rasterio/enums.py#L192-L200
-class PhotometricInterp(Enum):
-    black = "MINISBLACK"
-    white = "MINISWHITE"
-    rgb = "RGB"
-    cmyk = "CMYK"
-    ycbcr = "YCbCr"
-    cielab = "CIELAB"
-    icclab = "ICCLAB"
-    itulab = "ITULAB"
+class PhotometricInterpretation(Enum):
+    WHITE_IS_ZERO = "MINISWHITE"
+    BLACK_IS_ZERO = "MINISBLACK"
+    RGB = "RGB"
+    RGBPALETTE = "PALETTE"
+    TRANSPARENCY_MASK = "MASK"
+    CMYK = "CMYK"
+    YCBCR = "YCbCr"
+    CIELAB = "CIELAB"
