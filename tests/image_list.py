14cb1a040cd17fa18581b5cbd1ab56e0518ba0f3
Kyle Barron <kyle@developmentseed.org>
2026-02-05 11:53:20 -0500

    Add LERC support

--- tests/image_list.py
+++ tests/image_list.py
@@ -6,13 +6,16 @@ if TYPE_CHECKING:
     from tests.conftest import Variant
 
 ALL_DATA_IMAGES: list[tuple[str, Variant]] = [
+    ("float32_1band_lerc_block32", "rasterio"),
+    ("float32_1band_lerc_deflate_block32", "rasterio"),
+    ("float32_1band_lerc_zstd_block32", "rasterio"),
+    ("nlcd_landcover", "nlcd"),
     ("uint16_1band_lzw_block128_predictor2", "rasterio"),
     ("uint8_1band_deflate_block128_unaligned", "rasterio"),
-    ("uint8_rgb_deflate_block64_cog", "rasterio"),
     ("uint8_1band_lzma_block64", "rasterio"),
+    ("uint8_rgb_deflate_block64_cog", "rasterio"),
     ("uint8_rgb_webp_block64_cog", "rasterio"),
     ("uint8_rgba_webp_block64_cog", "rasterio"),
-    ("nlcd_landcover", "nlcd"),
 ]
 """All fixtures where the data can be compared with rasterio."""
 
@@ -21,8 +24,6 @@ ALL_TEST_IMAGES: list[tuple[str, Variant]] = [
     *ALL_DATA_IMAGES,
     # YCbCr is auto-decompressed by rasterio
     ("maxar_opendata_yellowstone_visual", "vantor"),
-    # we don't support LERC yet
-    ("float32_1band_lerc_block32", "rasterio"),
 ]
 """All fixtures where we test metadata parsing."""
 
