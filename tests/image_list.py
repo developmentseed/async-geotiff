b23f40daa5c4377c2a2c6ead598b7819bc526838
Kyle Barron <kyle@developmentseed.org>
2026-02-05 13:16:04 -0500

    Add test with band-interleaved data

--- tests/image_list.py
+++ tests/image_list.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 ALL_DATA_IMAGES: list[tuple[str, str]] = [
+    ("eox_cloudless", "eox"),
+    ("nlcd_landcover", "nlcd"),
     ("uint16_1band_lzw_block128_predictor2", "rasterio"),
     ("uint8_1band_deflate_block128_unaligned", "rasterio"),
-    ("uint8_rgb_deflate_block64_cog", "rasterio"),
     ("uint8_1band_lzma_block64", "rasterio"),
+    ("uint8_rgb_deflate_block64_cog", "rasterio"),
     ("uint8_rgb_webp_block64_cog", "rasterio"),
     ("uint8_rgba_webp_block64_cog", "rasterio"),
-    ("nlcd_landcover", "nlcd"),
 ]
 """All fixtures where the data can be compared with rasterio."""
 
