1cf793df2fd597d463b87c8d29cb250f1799d19b
Kyle Barron <kyle@developmentseed.org>
2026-02-26 17:00:43 -0500

    feat: Add `GeoTIFF.colorinterp` (#105)
    * define enum
    
    * Start implementing colorinterp
    
    * impl tests
    
    * palette colorinterp
    
    * Return gray
    
    * Update for cmyk and ycbcr
    
    * Cleaner colorinterps for rgb with more than 3 bands
    
    * cleaner input api
    
    * comment

---
+++ src/async_geotiff/_colorinterp.py
@@ -0,0 +1,72 @@
+# ruff: noqa: PLR2004
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .enums import ColorInterp, PhotometricInterpretation
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+def infer_color_interpretation(  # noqa: PLR0911
+    *,
+    count: int,
+    photometric: PhotometricInterpretation | None,
+    # TODO: update to enum when available in async-tiff
+    # https://github.com/developmentseed/async-tiff/issues/252
+    extra_samples: Sequence[int],
+) -> tuple[ColorInterp, ...]:
+    """Infer colorinterp array based on GeoTIFF metadata."""
+    match photometric:
+        case None:
+            return (ColorInterp.UNDEFINED,) * count
+        case PhotometricInterpretation.BLACK_IS_ZERO:
+            return (ColorInterp.GRAY,) * count
+        case PhotometricInterpretation.RGB:
+            if count <= 2:
+                raise NotImplementedError(
+                    "RGB photometric interpretation with fewer than 3 bands "
+                    "is not supported.",
+                )
+
+            if count == 3:
+                return (
+                    ColorInterp.RED,
+                    ColorInterp.GREEN,
+                    ColorInterp.BLUE,
+                )
+
+            if count >= 4:
+                # Color interpretations for any extra samples
+                # Sample = 2 means alpha, otherwise we mark it as undefined
+                # https://web.archive.org/web/20240329145321/https://www.awaresystems.be/imaging/tiff/tifftags/extrasamples.html
+                extra_colorinterps = [
+                    ColorInterp.ALPHA if sample == 2 else ColorInterp.UNDEFINED
+                    for sample in extra_samples or []
+                ]
+                return (
+                    ColorInterp.RED,
+                    ColorInterp.GREEN,
+                    ColorInterp.BLUE,
+                    *extra_colorinterps,
+                )
+
+        case PhotometricInterpretation.RGBPALETTE:
+            return (ColorInterp.PALETTE,)
+        case PhotometricInterpretation.CMYK:
+            return (
+                ColorInterp.CYAN,
+                ColorInterp.MAGENTA,
+                ColorInterp.YELLOW,
+                ColorInterp.BLACK,
+            )
+        case PhotometricInterpretation.YCBCR:
+            return (
+                ColorInterp.Y,
+                ColorInterp.Cb,
+                ColorInterp.Cr,
+            )
+
+    raise NotImplementedError
---
+++ tests/test_colorinterp.py
@@ -0,0 +1,33 @@
+"""Test fetching tiles from a GeoTIFF."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from .image_list import ALL_DATA_IMAGES
+
+if TYPE_CHECKING:
+    from .conftest import LoadGeoTIFF, LoadRasterio
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("variant", "file_name"),
+    ALL_DATA_IMAGES,
+)
+async def test_colorinterp(
+    load_geotiff: LoadGeoTIFF,
+    load_rasterio: LoadRasterio,
+    variant: str,
+    file_name: str,
+) -> None:
+    geotiff = await load_geotiff(file_name, variant=variant)
+    colorinterp = geotiff.colorinterp
+    assert colorinterp is not None, "Expected color interpretation to be present."
+
+    with load_rasterio(file_name, variant=variant) as rasterio_ds:
+        rasterio_colorinterp = rasterio_ds.colorinterp
+
+    assert rasterio_colorinterp == colorinterp
