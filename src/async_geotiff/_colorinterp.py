cd14af7386481f12518b5ef849894fcd58168473
Kyle Barron <kyle@developmentseed.org>
2026-02-26 16:38:35 -0500

    palette colorinterp

--- src/async_geotiff/_colorinterp.py
+++ src/async_geotiff/_colorinterp.py
@@ -12,26 +12,31 @@ if TYPE_CHECKING:
 
 def infer_color_interpretation(geotiff: GeoTIFF) -> tuple[ColorInterp, ...]:
     primary_ifd = geotiff._primary_ifd  # noqa: SLF001
-    if geotiff.photometric == PhotometricInterpretation.RGB:
-        if len(primary_ifd.sample_format) == 3:
-            return (
-                ColorInterp.RED,
-                ColorInterp.GREEN,
-                ColorInterp.BLUE,
-            )
 
-        if len(primary_ifd.sample_format) == 4:
-            if primary_ifd.extra_samples == [2]:
+    match geotiff.photometric:
+        case PhotometricInterpretation.RGB:
+            if len(primary_ifd.sample_format) == 3:
                 return (
                     ColorInterp.RED,
                     ColorInterp.GREEN,
                     ColorInterp.BLUE,
-                    ColorInterp.ALPHA,
                 )
 
-            raise NotImplementedError(
-                "Only RGBA with associated alpha (extra_samples=2) "
-                "is supported for RGB photometric interpretation.",
-            )
+            if len(primary_ifd.sample_format) == 4:
+                if primary_ifd.extra_samples == [2]:
+                    return (
+                        ColorInterp.RED,
+                        ColorInterp.GREEN,
+                        ColorInterp.BLUE,
+                        ColorInterp.ALPHA,
+                    )
+
+                raise NotImplementedError(
+                    "Only RGBA with associated alpha (extra_samples=2) "
+                    "is supported for RGB photometric interpretation.",
+                )
+
+        case PhotometricInterpretation.RGBPALETTE:
+            return (ColorInterp.PALETTE,)
 
     raise NotImplementedError
